e68f691de5774bf3bee0c3f7db75a7d8179b94f2
Joao Bonchristiano <jp.bonchristiano@gmail.com>
2022-10-25 15:43:46 -0400

    Added links to the buttons

--- buzzeats/src/App.js
+++ buzzeats/src/App.js
@@ -3,11 +3,16 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import NavBar from "./NavBar";
+import { Form } from "react-router-dom";
 import Restaurant from "./Restaurant";
 import restaurants from "./restaurants.json";
-import dining from "./dining.jpg"
+import dining from "./dining.jpg";
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
 
 function App() {  
+  const myRef = useRef(null)
+  const navigate = useNavigate();
   return (
     <div>
       {/* <NavBar /> */}
@@ -16,13 +21,14 @@ function App() {
       <img src={dining} alt="GT Dining" className="diningImage"></img>
       <div class="titleAndButtonContainer">
       <h1 className="mainTitle">BuzzEats</h1>
-      <div class="buttonContainer">
-      <button class="homepageButton">Restaurants</button>
-      <button class="homepageButton">About Us</button>  
+      <div className="buttonContainer">
+      <input className="searchBar"></input>
+      <button className="homepageButton" onClick={() => myRef.current.scrollIntoView()}>Restaurants</button>
+      <button className="homepageButton" onClick={() => navigate("/aboutus")}>About Us</button>  
       </div>     
       </div>
 
-      <h2 style={{textAlign:"center", marginTop:"30px"}}>Restaurants</h2>
+      <h2 style={{textAlign:"center", marginTop:"30px"}} ref={myRef}>Restaurants</h2>
       <div className="restaurantContainer">
       {restaurants.map((restaurant, i) => (
         <Restaurant key={i} data={restaurant}></Restaurant>
