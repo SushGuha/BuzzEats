3e917104f271c4ecc3c27d83fe955fcdf6b0de1e
Zini Chakraborty <zinichakraborty@gmail.com>
2022-11-26 12:45:41 -0800

    Comments Add/Delete

--- buzzeats/src/App.js
+++ buzzeats/src/App.js
@@ -10,7 +10,9 @@ import dining from "./dining.jpg";
 import { useNavigate } from "react-router-dom";
 import { useRef } from "react";
 import RestaurantPage from "./RestaurantPage";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
+import {db} from "./firebase-config";
 
 function App() {  
   const myRef = useRef(null)
@@ -18,6 +20,21 @@ function App() {
   const [showCover, setShowCover] = useState(false);
   const [data, setData] = useState({});
   const [nameFilter, setNameFilter] = useState('');
+  const [postLists, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    };
+    getPosts();
+  });
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+  }
+
   return (
     <div style={{marginBottom: "600px"}}>
       {showCover && (
@@ -34,9 +51,9 @@ function App() {
       <button className="homepageButton" onClick={() => myRef.current.scrollIntoView()}>Restaurants</button>
       <button className="homepageButton" onClick={() => navigate("/aboutus")}>About Us</button>  
       <button className="homepageButton" onClick={() => navigate("/map")}>Map</button> 
+      <button className="homepageButton" onClick={() => navigate("/createpost")}>Create Post</button>
       </div>
       </div>
-
       <h2 style={{textAlign:"center", marginTop:"30px"}} ref={myRef}>Restaurants</h2>
       <div style={{textAlign:"center"}}><input className="searchBar" onChange={(event) => setNameFilter(event.target.value)}></input></div>
       <div className="restaurantContainer">
@@ -45,7 +62,25 @@ function App() {
         <Restaurant key={i} data={restaurant} displayRestaurantPage={() => {setShowCover(true); setData(restaurant)}}></Restaurant>
         )
         ))}
+      <h2 style={{textAlign:"center", marginTop:"30px"}} ref={myRef}>Posts</h2>
       </div>
+      {postLists.map((post) =>{
+        return <div className="post">
+          {" "}
+          <div className="postHeader">
+            {" "}
+            <div className="title">
+              <h3> {post.title} </h3>
+            </div>
+            <div className="deletePost">
+              <button onClick={() => {deletePost(post.id)}}> &#128465; </button>
+            </div>
+          </div>
+          <div className="postTextContainer"> 
+            {post.postText}
+          </div>
+        </div>
+      })};
     </div>
   );
 }
--- buzzeats/src/index.js
+++ buzzeats/src/index.js
@@ -7,7 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutUs from "./AboutUs";
 import FoodMap from "./FoodMap";
-
+import CreatePost from "./createpost";
+import Login from "./login";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,6 +18,8 @@ root.render(
         <Route path="/" element={<App />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/map" element={<FoodMap />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </BrowserRouter>
    </React.StrictMode>
