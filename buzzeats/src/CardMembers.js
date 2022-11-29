489d7f262b2b9680c5024d9967ca1751142c68d1
mehdibencheqroun <mehdibencheqroun04@gmail.com>
2022-11-28 21:44:47 -0500

    About us page - work still in progress

---
+++ buzzeats/src/CardMembers.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import "./CardMembersStyles.css";
+
+class CardsMembers extends Component {
+    render(){
+        return (
+            <section class="team-area">
+            <div class="container">
+            <div class="row">
+               <div class="col-lg-3 col-md-6">
+                  <div class="single-team text-center team-style-one">
+                     <div class="team-image">
+                        <img src={this.props.imagee} alt="Team" />
+                     </div>
+                     <div class="team-content">
+                        <h4 class="name">{this.props.name}</h4>
+                        <span class="sub-title">{this.props.description}</span>
+                     </div>
+                  </div>
+               </div>
+               </div>
+               </div>
+               </section>
+  )
+    }
+};
+
+export default CardsMembers;
