691aeea5d19acf0ebbffcfb0f579a7283ad968aa
Sushant Guha <sush.guha@gmail.com>
2022-11-29 19:15:46 -0500

    AboutUS page changes - Mehdi

--- buzzeats/src/FoodMap.js
+++ buzzeats/src/FoodMap.js
@@ -6,7 +6,7 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "./FoodMap.css";
 import { db } from "./firebase-config";
 import restaurants from "./restaurants.json";
-const api_key = `6d211782f0ac84f1355cf494088c24247854e663`;
+const api_key = `84f74f42535cdbffcebd9625bf9e532f181877d7`;
 const markerIcon = new L.Icon({
   iconUrl: require("./marker.png"),
   iconSize: [40, 45],
@@ -21,22 +21,22 @@ const requestOptions = {
 function FoodMap() {
   const [gCodes, setGCodes] = useState(new Map([]));
 
-  for (let i = 0; i < restaurants.length; i++) {
-    let q = `https://api.geocodify.com/v2/geocode?api_key=${api_key}&q=${restaurants[i].location}`;
-    // console.log("Query: ", q);
-    const id = restaurants[i].hours;
-    fetch(q, requestOptions)
-      .then((out) => out.json())
-      .then((data) => {
-        if (!data.response.features[0]) {
-          return;
-        }
-        let coords = data.response.features[0].geometry.coordinates;
-        // console.log("here ", q, [coords[1], coords[0]]);
-        let coords2 = [coords[1], coords[0]]; // lat, lng
-        setGCodes(new Map(gCodes.set(id, coords2)));
-      });
-  }
+  // for (let i = 0; i < restaurants.length; i++) {
+  //   let q = `https://api.geocodify.com/v2/geocode?api_key=${api_key}&q=${restaurants[i].location}`;
+  //   // console.log("Query: ", q);
+  //   const id = restaurants[i].hours;
+  //   fetch(q, requestOptions)
+  //     .then((out) => out.json())
+  //     .then((data) => {
+  //       if (!data.response.features[0]) {
+  //         return;
+  //       }
+  //       let coords = data.response.features[0].geometry.coordinates;
+  //       // console.log("here ", q, [coords[1], coords[0]]);
+  //       let coords2 = [coords[1], coords[0]]; // lat, lng
+  //       setGCodes(new Map(gCodes.set(id, coords2)));
+  //     });
+  // }
 
   // useEffect(() => {
   //   if (!restaurants) {
@@ -58,33 +58,11 @@ function FoodMap() {
           attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
           url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
         />
-        {restaurants &&
-          gCodes &&
-          restaurants.map((ev, idx) => {
-            const c = gCodes.get(ev.hours);
-            if (!c) {
-              return <span key={idx}></span>;
-            }
-            return (
-              <Marker key={idx} position={c} icon={markerIcon}>
-                <Popup
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                  key={idx}
-                >
-                  <h2>{ev.name}</h2>
-                  <p>Rating: {ev.rating}</p>
-                  <p>Cuisine: {ev.cuisine}</p>
-                  <p>Description: {ev.descr}</p>
-                  <p>Location: {ev.location}</p>
-                </Popup>
-              </Marker>
-            );
-          })}
+        <Marker position={[33.77776, -84.39563]} icon={markerIcon}>
+          <Popup>
+            <h2>Sideways Cafe</h2>
+          </Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
