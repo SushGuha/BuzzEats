de4286cac2f8fcd32d9f73fbb4dd735be5658f31
Sushant Guha <sush.guha@gmail.com>
2022-12-19 12:19:16 +0530

    Creating post fixed.

--- buzzeats/src/App.js
+++ buzzeats/src/App.js
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import NavBar from "./NavBar";
+import postLists from "./posts.json";
 import { Form } from "react-router-dom";
 import Restaurant from "./Restaurant";
 import restaurants from "./restaurants.json";
@@ -11,21 +12,21 @@ import { useNavigate } from "react-router-dom";
 import { useRef } from "react";
 import RestaurantPage from "./RestaurantPage";
 import { useState, useEffect } from "react";
-import {getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
-import {db} from "./firebase-config";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { db } from "./firebase-config";
 
-function App() {  
-  const myRef = useRef(null)
+function App() {
+  const myRef = useRef(null);
   const navigate = useNavigate();
   const [showCover, setShowCover] = useState(false);
   const [data, setData] = useState({});
-  const [nameFilter, setNameFilter] = useState('');
+  const [nameFilter, setNameFilter] = useState("");
   const [postLists, setPostList] = useState([]);
   const postsCollectionRef = collection(db, "posts");
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(postsCollectionRef);
-      setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getPosts();
   });
@@ -33,54 +34,100 @@ function App() {
   const deletePost = async (id) => {
     const postDoc = doc(db, "posts", id);
     await deleteDoc(postDoc);
-  }
+  };
 
   return (
-    <div style={{marginBottom: "600px"}}>
+    <div style={{ marginBottom: "600px" }}>
       {showCover && (
-        <div className={showCover?'fadeIn':'fadeOut'} >
-      <RestaurantPage data={data} hideCover={() => setShowCover(false)}></RestaurantPage></div>)}
-      
+        <div className={showCover ? "fadeIn" : "fadeOut"}>
+          <RestaurantPage
+            data={data}
+            hideCover={() => setShowCover(false)}
+          ></RestaurantPage>
+        </div>
+      )}
       <div className="darkness"></div>
-      
       <img src={dining} alt="GT Dining" className="diningImage"></img>
       <div className="titleAndButtonContainer">
-      <h1 className="mainTitle">BuzzEats</h1>
-      <div className="buttonContainer">
-      
-      <button className="homepageButton" onClick={() => myRef.current.scrollIntoView()}>Restaurants</button>
-      <button className="homepageButton" onClick={() => navigate("/aboutus")}>About Us</button>  
-      <button className="homepageButton" onClick={() => navigate("/map")}>Map</button> 
-      <button className="homepageButton" onClick={() => navigate("/createpost")}>Create Post</button>
-      </div>
-      </div>
-      <h2 style={{textAlign:"center", marginTop:"30px"}} ref={myRef}>Restaurants</h2>
-      <div style={{textAlign:"center"}}><input className="searchBar" onChange={(event) => setNameFilter(event.target.value)}></input></div>
-      <div className="restaurantContainer">
-      {restaurants.map((restaurant, i) => (
-        restaurant.name.toLowerCase().includes(nameFilter.toLowerCase()) && (
-        <Restaurant key={i} data={restaurant} displayRestaurantPage={() => {setShowCover(true); setData(restaurant)}}></Restaurant>
-        )
-        ))}
-      <h2 style={{textAlign:"center", marginTop:"30px"}} ref={myRef}>Posts</h2>
-      </div>
-      {postLists.map((post) =>{
-        return <div className="post">
-          {" "}
-          <div className="postHeader">
-            {" "}
-            <div className="title">
-              <h3> {post.title} </h3>
-            </div>
-            <div className="deletePost">
-              <button onClick={() => {deletePost(post.id)}}> &#128465; </button>
-            </div>
-          </div>
-          <div className="postTextContainer"> 
-            {post.postText}
-          </div>
+        <h1 className="mainTitle">BuzzEats</h1>
+        <div className="buttonContainer">
+          <button
+            className="homepageButton"
+            onClick={() => myRef.current.scrollIntoView()}
+          >
+            Restaurants
+          </button>
+          <button
+            className="homepageButton"
+            onClick={() => navigate("/aboutus")}
+          >
+            About Us
+          </button>
+          <button className="homepageButton" onClick={() => navigate("/map")}>
+            Map
+          </button>
+          <button
+            className="homepageButton"
+            onClick={() => navigate("/createpost")}
+          >
+            Create Post
+          </button>
         </div>
-      })};
+      </div>
+      <h2 style={{ textAlign: "center", marginTop: "30px" }} ref={myRef}>
+        Restaurants
+      </h2>
+      <div style={{ textAlign: "center" }}>
+        <input
+          className="searchBar"
+          onChange={(event) => setNameFilter(event.target.value)}
+        ></input>
+      </div>
+      <div className="restaurantContainer">
+        {restaurants.map(
+          (restaurant, i) =>
+            restaurant.name
+              .toLowerCase()
+              .includes(nameFilter.toLowerCase()) && (
+              <Restaurant
+                key={i}
+                data={restaurant}
+                displayRestaurantPage={() => {
+                  setShowCover(true);
+                  setData(restaurant);
+                }}
+              ></Restaurant>
+            )
+        )}
+        <h2 style={{ textAlign: "center", marginTop: "30px" }} ref={myRef}>
+          Posts
+        </h2>
+      </div>
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            {" "}
+            <div className="postHeader">
+              {" "}
+              <div className="title">
+                <h3> {post.title} </h3>
+              </div>
+              <div className="deletePost">
+                <button
+                  onClick={() => {
+                    deletePost(post.id);
+                  }}
+                >
+                  {" "}
+                  &#128465;{" "}
+                </button>
+              </div>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+          </div>
+        );
+      })}
+      ;
     </div>
   );
 }
--- buzzeats/src/createpost.js
+++ buzzeats/src/createpost.js
@@ -1,44 +1,46 @@
 import React, { useState } from "react";
-import './App.css'
-import { addDoc, collection } from 'firebase/firestore';
+import "./App.css";
+import { addDoc, collection } from "firebase/firestore";
 import { db } from "./firebase-config";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function CreatePost() {
-    const [title, setTitle] = useState("");
-    const [postText, setPostText] = useState("");
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("");
 
-    const postsCollectionRef = collection(db, "posts");
-    let navigate = useNavigate();
-    const createPost = async () => {
-        await addDoc(postsCollectionRef, {title, postText});
-        navigate("/");
-    };
-    
-    return <div className="createPostPage"> 
-        <div className="cpContainer">
-            <h1>Create A Post</h1>
-            <div className="inputGp">
-                <label>Title:</label>
-                <input 
-                    placeholder="Title..."
-                    onChange={(event) => {
-                        setTitle(event.target.value);
-                    }}
-                />
-            </div>
-            <div className="inputGp">
-                <label>Post:</label>
-                <textarea
-                    placeholder="Post..."
-                    onChange={(event) => {
-                        setPostText(event.target.value);
-                    }}
-                />
-            </div>
-            <button onClick={createPost}>Submit Post</button>
+  const postsCollectionRef = collection(db, "posts");
+  let navigate = useNavigate();
+  const createPost = async () => {
+    await addDoc(postsCollectionRef, { title, postText });
+    navigate("/");
+  };
+
+  return (
+    <div className="createPostPage">
+      <div className="cpContainer">
+        <h1>Create A Post</h1>
+        <div className="inputGp">
+          <label>Title:</label>
+          <input
+            placeholder="Title..."
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
         </div>
+        <div className="inputGp">
+          <label>Post:</label>
+          <textarea
+            placeholder="Post..."
+            onChange={(event) => {
+              setPostText(event.target.value);
+            }}
+          />
+        </div>
+        <button onClick={createPost}>Submit Post</button>
+      </div>
     </div>
+  );
 }
 
 export default CreatePost;
